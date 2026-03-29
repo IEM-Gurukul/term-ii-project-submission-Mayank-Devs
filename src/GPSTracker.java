4c851dfd2ea0ad1235acb0fe4f6dc265de932401
Mayank Roy <Mayank.Roy2024@iem.edu.in>
2026-03-29 11:56:54 +0530

    completed README with project details

--- src/GPSTracker.java
+++ src/GPSTracker.java
@@ -3,7 +3,7 @@ package src;
 class GPSTracker {
     public void updateLocation(Vehicle vehicle, Location location) {
         System.out.println("Vehicle " + vehicle.getVehicleId() +
-                " is now at " + location.getPlace())
+                " is now at " + location.getPlace());
     }
 }
     
